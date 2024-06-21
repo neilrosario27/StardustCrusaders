0485af29400b0d009c4130ca12d1e1486ad627d2
neilrosario27 <neilrosario27@gmail.com>
2024-06-21 19:11:53 +0530

    navbar

---
+++ frontend/my-app/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css"; // Assuming you will add some CSS styling later
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <button className="nav-button">1</button>
+      <button className="nav-button">2</button>
+      <button className="nav-button">3</button>
+      <button className="nav-button">4</button>
+    </nav>
+  );
+};
+
+export default Navbar;
